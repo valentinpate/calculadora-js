bccb0a28f5cfa3a8e886fea21fbef575c687df0b
valentinpate <valentinp261@gmail.com>
2023-08-13 21:02:50 -0300

    commit

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+const calculos = require("./funciones")
+
+let numero1 = 1
+let numero2 = 1
+
+console.log(calculos.suma(numero1, numero2))
+console.log(calculos.resta(numero1, numero2))
+console.log(calculos.multi(numero1, numero2))
+console.log(calculos.divis(numero1, numero2))
---
+++ funciones.js
@@ -0,0 +1,21 @@
+function suma(a, b){
+    let resultadoSuma = a + b
+    return resultadoSuma
+}
+
+function resta(a, b){
+    let resultadoResta = a - b
+    return resultadoResta
+}
+
+function multi(a, b){
+    let resultadoMulti = a * b
+    return resultadoMulti
+}
+
+function divis(a, b){
+    let resultadoDiv = a / b
+    return resultadoDiv
+}
+
+module.exports = {suma, resta, multi, divis}
